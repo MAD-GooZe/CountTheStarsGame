e12a981143b4b4053d09e7b98bbba7ce1df45c1b
MAD-GooZe <mad.gooze@gmail.com>
2014-01-21 22:19:46 +0400

    logo url fixed

--- js/stars.js
+++ js/stars.js
@@ -284,7 +284,7 @@ $(document).ready(function(){
     $(document).on('click', '.social_share', function(){
         Share.go(this, {
             text:  "I've counted " + starsCounted + " star" + (starsCounted % 10 == 1 ? "s" : "") + " in \"Count The Stars\" game.",
-            image: window.location.host + "/img/logo.png"
+            image: "http://mad-gooze.github.io/CountTheStarsGame/img/logo.png"
         });
     });
 });
